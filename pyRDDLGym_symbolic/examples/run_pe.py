313c342377bd429f82762d58e27297982bf86fcd
Guanghan Wang <xuanghdu@outlook.com>
2024-05-25 22:02:44 -0400

    Upload refactored code and instructions

--- pyRDDLGym_symbolic/examples/run_pe.py
+++ pyRDDLGym_symbolic/examples/run_pe.py
@@ -1,3 +1,22 @@
+"""
+SIR domain:
+
+python pyRDDLGym_symbolic/examples/run_pe.py --domain SIR --policy_fpath=./pyRDDLGym_symbolic/examples/files/SIR/policy/p1.json --max_iter=4 --save_graph
+
+Racecar domain:
+
+python pyRDDLGym_symbolic/examples/run_pe.py --domain Racecar --policy_fpath=./pyRDDLGym_symbolic/examples/files/Racecar/policy/policy.json --max_iter=5 --save_graph
+
+The Racecar domain also has a square goal region domain apart from the default circular goal region. Uncomment the respective lines in the domain file to use the square goal region.
+
+Reservoir domain:
+
+python pyRDDLGym_symbolic/examples/run_pe.py --domain Reservoir --policy_fpath=./pyRDDLGym_symbolic/examples/files/Reservoir/policy/policy.json --max_iter=2 --save_graph
+
+
+Note that changing 'resolution' to a smaller value will increase the accuracy of the plot, but will also increase the computation time.
+"""
+
 """An example PE run."""
 
 import argparse
@@ -12,6 +31,11 @@ from pyRDDLGym_symbolic.mdp.mdp_parser import MDPParser
 from pyRDDLGym_symbolic.mdp.policy_parser import PolicyParser
 from pyRDDLGym_symbolic.solver.pe import PolicyEvaluation
 
+from tqdm import tqdm
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use("TkAgg")
 
 _DIR = 'pyRDDLGym_symbolic/examples/files/{domain}/'
 _DOMAIN_PATH = _DIR + 'domain.rddl'
@@ -43,7 +67,7 @@ def run_vi(args: argparse.Namespace):
     # XADD compilation
     xadd_model = RDDLModelXADD(model, reparam=False)
     xadd_model.compile()
-    
+
     mdp_parser = MDPParser()
     mdp = mdp_parser.parse(
         xadd_model,
@@ -70,6 +94,9 @@ def run_vi(args: argparse.Namespace):
     )
     res = pe_solver.solve()
 
+    # print(f"{model.cpfs=}")
+    # print(f"{mdp.context._id_to_node[22]}")
+
     # Export the solution to a file
     env_path = os.path.dirname(domain_file)
     sol_dir = os.path.join(env_path, 'sdp', 'pe')
@@ -87,6 +114,123 @@ def run_vi(args: argparse.Namespace):
             mdp.context.save_graph(value_dd, file_name=graph_fpath)
     print(f'Times per iterations: {res["time"]}')
 
+    var_set = mdp.context.collect_vars(res["value_dd"][-1])
+    print(f"{var_set=}")
+    var_dict = {}
+    for i in var_set:
+        var_dict[f"{i}"] = i
+    # print(f"{var_dict=}")
+
+    #########################################################################################################################
+    # Visualizations
+
+    def plot_and_save(Z, x, y, xlabel, ylabel, title, filename, show=False):
+        X, Y = np.meshgrid(x, y)
+        fig, ax = plt.subplots()
+        c = ax.pcolormesh(X, Y, Z, cmap="gray", shading="auto")
+        fig.colorbar(c, ax=ax)
+        ax.set_aspect("equal")
+        ax.set_title(title)
+        ax.set_xlabel(xlabel)
+        ax.set_ylabel(ylabel)
+        plt.savefig(filename, bbox_inches="tight", dpi=300)
+        if show:
+            plt.show()
+        plt.close()
+
+    if domain == "SIR":
+
+        resolution = 0.01
+        # the smaller the resolution, the more accurate the plot, but the slower the computation
+
+        infected = 1e-10
+        beta_values = np.arange(0, 2, resolution)
+
+        ## no vaccination (gamma vs beta)
+
+        gamma_values = np.arange(0, 2, resolution)
+        Z = np.zeros((len(gamma_values), len(beta_values)))
+
+        bool_assign = {var_dict["g"]: False, var_dict["overwhelmed"]: False}
+        cont_assign = {var_dict["infected"]: infected, var_dict["susceptible"]: 1 - infected, var_dict["vaccinated"]: 0}
+
+        for i, gemma_val in enumerate(tqdm(gamma_values)):
+            for j, beta_val in enumerate(beta_values):
+                cont_assign[var_dict["beta"]] = beta_val
+                cont_assign[var_dict["gamma"]] = gemma_val
+                Z[i][j] = mdp.context.evaluate(res["value_dd"][-1], bool_assign=bool_assign, cont_assign=cont_assign)
+
+        Z = Z.T
+
+        plot_and_save(Z, gamma_values, beta_values, "Recovery Rate", "Infection Rate", f"Value Function with no Vaccination and Initial Infected = {cont_assign[var_dict['infected']]}", f"imgs/SIR_{args.max_iter}_{resolution}_{infected}_no_vaccination.png", show=True)
+
+        ## with vaccination (vaccination vs beta with different gamma values)
+
+        vaccination_values = np.arange(0, 1, resolution)
+
+        for gamma in [5e-4, 5e-3, 5e-2, 5e-1]:
+
+            Z = np.zeros((len(vaccination_values), len(beta_values)))
+
+            bool_assign = {var_dict["g"]: False, var_dict["overwhelmed"]: False}
+            cont_assign = {var_dict["infected"]: infected, var_dict["susceptible"]: 1 - infected, var_dict["gamma"]: gamma}
+
+            for i, vaccination_val in enumerate(tqdm(vaccination_values)):
+                for j, beta_val in enumerate(beta_values):
+                    cont_assign[var_dict["beta"]] = beta_val
+                    cont_assign[var_dict["vaccinated"]] = vaccination_val
+                    Z[i][j] = mdp.context.evaluate(res["value_dd"][-1], bool_assign=bool_assign, cont_assign=cont_assign)
+
+            Z = Z.T
+
+            plot_and_save(Z, vaccination_values, beta_values, "Vaccination Rate", "Infection Rate", f"Value Function with Recovery Rate = {cont_assign[var_dict['gamma']]}", f"imgs/SIR_{args.max_iter}_{resolution}_{gamma}.png", show=True)
+
+    elif domain == "Racecar":
+
+        resolution = 0.5
+        # the smaller the resolution, the more accurate the plot, but the slower the computation
+
+        ax_noise = np.arange(-30, 30, resolution)
+        ay_noise = np.arange(-5, 250, resolution)
+
+        Z = np.zeros((len(ax_noise), len(ay_noise)))
+
+        bool_assign = {var_dict["reach"]: False, var_dict["violation"]: False}
+        cont_assign = {var_dict["ax"]: 0, var_dict["ay"]: 0, var_dict["x"]: 1, var_dict["y"]: 0, var_dict["vx"]: 0, var_dict["vy"]: 0.0}
+
+        for i, t1 in enumerate(tqdm(ax_noise)):
+            for j, t2 in enumerate(ay_noise):
+                cont_assign[var_dict["ax_noise"]] = t1
+                cont_assign[var_dict["ay_noise"]] = t2
+                Z[i][j] = mdp.context.evaluate(res["value_dd"][-1], bool_assign=bool_assign, cont_assign=cont_assign)
+
+        Z = Z.T
+
+        plot_and_save(Z, ax_noise, ay_noise, "ax noise", "ay noise", f"Value Function for {args.max_iter} steps", f"./imgs/Racecar_{args.max_iter}_{resolution}.png", show=True)
+
+    elif domain == "Reservoir":
+
+        resolution = 0.1
+        # the smaller the resolution, the more accurate the plot, but the slower the computation
+
+        rlevel___t1 = np.arange(42.5, 57.5, resolution)
+        rlevel___t2 = np.arange(42.5, 57.5, resolution)
+
+        Z = np.zeros((len(rlevel___t1), len(rlevel___t2)))
+
+        bool_assign = {var_dict["failure"]: False}
+        cont_assign = {}
+
+        for i, t1 in enumerate(tqdm(rlevel___t1)):
+            for j, t2 in enumerate(rlevel___t2):
+                cont_assign[var_dict["rlevel___t1"]] = t1
+                cont_assign[var_dict["rlevel___t2"]] = t2
+                Z[i][j] = mdp.context.evaluate(res["value_dd"][-1], bool_assign=bool_assign, cont_assign=cont_assign)
+
+        plot_and_save(1-Z.T, rlevel___t1, rlevel___t2, "rlevel___t1", "rlevel___t2", f"Value Function for {args.max_iter} steps", f"imgs/Reservoir_{args.max_iter}_{resolution}.png", show=True)
+
+    else:
+        raise ValueError("Domain not evaluated in the thesis")
 
 if __name__ == "__main__":
     import argparse
